032aa4d1c7b1bf15642f4c56ec62b1f43e9288e8
nangthok02 <nangthok02@gmail.com>
2023-03-19 15:36:24 +0700

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,74 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Always",
+    artist: "Bon Jovi",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Kesepian",
+    artist: "Melani & Rusdi",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Heaven",
+    artist: "Brian Adan",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Kangen",
+    artist: "Dewa 19",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "ALWAYS SOMEWHERE",
+    artist: "SCORPIONS",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Sugeng Dalu",
+    artist: "Denny caknan",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "She'is Gone",
+    artist: "Cover by Buble Dia",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Tatu",
+    artist: "Cover by Widiodari",
+    img: "music-8",
+    src: "music-8"
+  },
+{
+  name: "NovemberRain",
+    artist: "Guns Rooses",
+    img: "music-9",
+    src: "music-9"
+}
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
